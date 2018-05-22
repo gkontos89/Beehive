d4f300397bf31d4020612200eccaed28e1a04eae
George Kontos <gkontos89@gmail.com>
2018-05-21 22:10:46 -0500

    added career point test suite

--- app/src/androidTest/java/com/marshmallow/beehive/ProfileSetupCareerPointTest.java
+++ app/src/androidTest/java/com/marshmallow/beehive/ProfileSetupCareerPointTest.java
@@ -1,21 +1,30 @@
 package com.marshmallow.beehive;
 
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
 import com.marshmallow.beehive.models.CareerPointModel;
 import com.marshmallow.beehive.models.ModelManager;
 import com.marshmallow.beehive.ui.profileSetup.ProfileSetupCareerPointActivity;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.core.IsNot.not;
 
 /**
  * Created by George on 5/21/2018.
@@ -26,12 +35,59 @@ public class ProfileSetupCareerPointTest {
     @Rule
     public ActivityTestRule<ProfileSetupCareerPointActivity> profileSetupCareerPointActivityActivityTestRule = new ActivityTestRule<>(ProfileSetupCareerPointActivity.class);
 
+    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+        return new RecyclerViewMatcher(recyclerViewId);
+    }
+
     @Test
     public void testAddCareerPointAndSave() {
+        // Fill out career point data and validate against model
         CareerPointCreator careerPointCreator = new CareerPointCreator("Caterpillar", "Peoria, IL", "6/5/2011", "7/3/2017");
         careerPointCreator.fillView();
+
+        // Add a couple career positions
+        onView(withId(R.id.add_career_position_button)).perform(click());
+        CareerPositionCreator careerPositionCreator = new CareerPositionCreator("Associate", "6/5/2011", "7/1/2012", "Code monkey");
+        careerPositionCreator.fillView();
+        careerPositionCreator.saveCareerPosition();
+
+        // Validate career point view data has persisted
+        onView(withId(R.id.career_point_text)).check(matches(withText("Caterpillar")));
+        onView(withId(R.id.career_point_location_text)).check(matches(withText("Peoria, IL")));
+        onView(withId(R.id.start_date_text)).check(matches(withText("6/5/2011")));
+        onView(withId(R.id.end_date_text)).check(matches(withText("7/3/2017")));
+
+        onView(withId(R.id.add_career_position_button)).perform(click());
+        careerPositionCreator = new CareerPositionCreator("Senior", "7/2/2012", "8/15/2013", "Code gorilla");
+        careerPositionCreator.fillView();
+        careerPositionCreator.saveCareerPosition();
+
+        // Validate the career positions show up in the view
+        onView(withRecyclerView(R.id.career_positions_rv).atPosition(0)).check(matches(hasDescendant(withText("Associate"))));
+        onView(withRecyclerView(R.id.career_positions_rv).atPosition(1)).check(matches(hasDescendant(withText("Senior"))));
+
+        // Remove career position
+        onView(withId(R.id.career_positions_rv)).perform(RecyclerViewActions.actionOnItemAtPosition(0, new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return null;
+            }
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                View button = view.findViewById(R.id.remove_button);
+                button.performClick();
+            }
+        }));
+        onView(withId(R.id.career_positions_rv)).check(matches(not(hasDescendant(withText("Associate")))));
         onView(withId(R.id.save_button)).perform(click());
-        assertEquals(ModelManager.getInstance().getUserModel().getUserStory().getCareerPointModels().size(), 1);
+
+        // Validate model
         CareerPointModel careerPointModel = ModelManager.getInstance().getUserModel().getUserStory().getCareerPointModels().get(0);
         assertEquals(careerPointModel.getName(), "Caterpillar");
         assertEquals(careerPointModel.getLocation(), "Peoria, IL");
